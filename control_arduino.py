34695700a79d9dfcae2894a45a8c5007a76f2686
theo <liminsyun@gmail.com>
2025-04-09 19:08:39 +0800

    update arduino control

--- control_arduino.py
+++ control_arduino.py
@@ -15,6 +15,8 @@ class control_arduino:
         self.lock = threading.Lock() 
         self.running = False # 
 
+        self.teaprature_history = []
+
         try:
             self.arduino = serial.Serial(com_port, baudrate, timeout=100) # 設定 timeout
             self.arduino.reset_input_buffer()
@@ -27,7 +29,7 @@ class control_arduino:
             print(f"Serial exception: {e}")
             self.arduino = None
             self.running = False
-
+    
     def _read_serial(self):
         while self.running and self.arduino:
             try:
@@ -46,6 +48,8 @@ class control_arduino:
                                         with self.lock:
                                             self.latest_temp_1 = temp1
                                             self.latest_temp_2 = temp2
+                                            self._append_tempature_history(np.array([temp1, temp2]))
+                                            
                                     except ValueError:
                                         print(f"ValueError parsing temps: {parts[1]}") 
                                         pass 
@@ -67,12 +71,24 @@ class control_arduino:
 
         print("Serial reading thread finished.")
     
+    def _append_tempature_history(self, tempature):
+        if len(self.teaprature_history) >= 100:
+            self.teaprature_history.pop(0)
+        self.teaprature_history.append(tempature)
+        
+    def return_tempature_history(self):
+        if len(self.teaprature_history) == 0:
+            print("No tempature history")
+            return np.array([-1.0, -1.0])
+        else:
+            return np.array(self.teaprature_history)
+    
     def control_arduino_and_return_temp(self, control_pwm_value):
         """
         control_pwm_value: 0~255
         will send to arduino to modify the pwm output
         and it will return the tempature of the sensor
-        return type: np.array([temp_value_1, temp_value_2])
+        return type: np.array([temp_1_return, temp_2_return])
         """
         if not self.arduino or not self.running:
             print("Arduino not connected or running.")
@@ -85,9 +101,9 @@ class control_arduino:
             self.arduino.write(command.encode("utf-8")) # send the command to arduino
 
             with self.lock:
-                temp_value_1 = self.latest_temp_1
-                temp_value_2 = self.latest_temp_2
-            return np.array([temp_value_1, temp_value_2])
+                temp_1_return = self.latest_temp_1
+                temp_2_return = self.latest_temp_2
+            return np.array([temp_1_return, temp_2_return])
         
         except serial.SerialException as e:
             print(f"Serial write error: {e}")
@@ -110,8 +126,21 @@ class control_arduino:
             current_temp_1 = self.latest_temp_1
             current_temp_2 = self.latest_temp_2
         return np.array([current_temp_1, current_temp_2])
+           
+    def control_arduino(self,  control_pwm_value):
+        '''
+        control the arduino by setting the pwm value
+        '''
+        if not self.arduino or not self.running:
+
+            try:
+                pwm_value = int(control_pwm_value)
+                pwm_value = max(0, min(pwm_value, 255)) 
+                command = f"PWM:{pwm_value}\n"
+                self.arduino.write(command.encode("utf-8"))
+            except serial.SerialException as e:
+                print(f"Serial write error: {e}")
         
-    
     def close(self):
         self.running = False 
         if hasattr(self, 'read_thread') and self.read_thread.is_alive():
@@ -134,17 +163,24 @@ class control_arduino:
         else:
              print("Cannot reset, Arduino not connected.")
             
-
-
 if __name__ == "__main__":
     '''
     only for test the control_arduino class
     '''
     arduino = control_arduino("COM7", 9600)
-    for i in range(1000):
+    for i in range(100):
         print(time.time())
         print(arduino.control_arduino_and_return_temp(100))
-        time.sleep(0.5)
+        time.sleep(0.1)
+
+    temp = arduino.return_tempature_history()
+    print(temp)
+
+    arduino.close()
+    print("End of program")
+    print("Arduino closed")
+
+    
     
 
 
