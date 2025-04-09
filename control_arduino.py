4900c3712f84fa3dc9607c7fdb69dde6b52ee3a6
theo <liminsyun@gmail.com>
2025-04-09 17:24:11 +0800

    update arduino control

--- control_arduino.py
+++ control_arduino.py
@@ -1,5 +1,6 @@
 import serial
 import time 
+import numpy as np
 
 """
 PC =======[PWM:{pwm value}]=======> Arduino
@@ -20,22 +21,27 @@ class control_arduino:
         control_pwm_value: 0~255
         will send to arduino to modify the pwm output
         and it will return the tempature of the sensor
+        return type: np.array([temp_value_1, temp_value_2])
         """
-        temp_value = -1
+        temp_value_1 = -1
+        temp_value_2 = -1
+
         try:
             # self.arduino.write("TEMP\n".encode())
             if self.arduino.in_waiting >= 0:
                 data = self.arduino.readline().decode("utf-8").strip()
-                print("data:" + str(data))
+                # print("data:" + str(data))
                 if "TEMP:" in data:
                     try:
-                        temp_value = float(data.split(":")[1].strip())
+                        temp_value_str = data.split(":")[1]
+                        temp_value_1 = float(temp_value_str.split(",")[0].strip())
+                        temp_value_2 = float(temp_value_str.split(",")[1].strip())
                     except ValueError as e:
                         print("value error:" + e)
                     pwm_value = control_pwm_value
                     self.arduino.write(f"PWM:{pwm_value}\n".encode("utf-8"))
                     current_time = time.time()
-            return temp_value
+            return np.array([temp_value_1, temp_value_2])
 
         except Exception as e:
             print("Serial error:", e)
@@ -43,20 +49,27 @@ class control_arduino:
     def return_tempure(self):
         '''
         only return the tempature of the sensor
+        return type: np.array([temp_value_1, temp_value_2])
         '''
-        temp_value = -1
+        temp_value_1 = -1
+        temp_value_2 = -1
+
         self.arduino.reset_input_buffer()
         try:
             # self.arduino.write("TEMP\n".encode())
             if self.arduino.in_waiting >= 0:
                 data = self.arduino.readline().decode("utf-8").strip()
-                print("data:" + str(data))
+                # print("data:" + str(data))
                 if "TEMP:" in data:
                     try:
-                        temp_value = float(data.split(":")[1].strip())
+                        temp_value_str = data.split(":")[1]
+                        temp_value_1 = float(temp_value_str.split(",")[0].strip())
+                        temp_value_2 = float(temp_value_str.split(",")[1].strip())
                     except ValueError as e:
                         print("value error:" + e)
-            return temp_value
+
+            return np.array([temp_value_1, temp_value_2])
+        
         except Exception as e:
             print("Serial error:", e)
     
@@ -76,6 +89,7 @@ if __name__ == "__main__":
     '''
     arduino = control_arduino("COM7", 9600)
     for i in range(100):
+        print(time.time())
         print(arduino.control_arduino_and_return_temp(100))
         time.sleep(0.5)
     
